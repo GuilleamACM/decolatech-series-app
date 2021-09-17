df8f8cf11873d652b8ff21ac74f15f713cad9d52
Guilherme Melo <gacm@cin.ufpe.br>
2021-09-17 16:53:06 -0300

    Implementing Series Genre enum

---
+++ Enum/Genre.cs
@@ -0,0 +1,19 @@
+﻿namespace Series.Enum
+{
+    public enum Genre
+    {
+        Action = 1,
+        Adventure = 2,
+        Comedy = 3,
+        Documentary = 4,
+        Drama = 5,
+        Fantasy = 6,
+        Horror = 7,
+        Musical = 8,
+        Romance = 9,
+        SciFi = 10,
+        Spy = 11,
+        Thriller = 12,
+        Western = 13,
+    }
+}
