4bc4cb1a8f1c6d4b96a1ea3c47126fe11ff42db9
Guilherme Melo <gacm@cin.ufpe.br>
2021-09-17 17:00:20 -0300

    Adding basic implementation for BaseEntity and Series

---
+++ Classes/Series.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Series.Enum;
+
+namespace Series.Classes
+{
+    public class Series : BaseEntity
+    {
+        public Genre Genre { get; private set; }
+        public string Title { get; private set; }
+        public int Year { get; private set; }
+        public string Description { get; private set; }
+        public bool Deleted { get; private set; }
+
+        public Series(int id, Genre genre, string title, string description, int year)
+        {
+            this.Id = id;
+            this.Genre = genre;
+            this.Title = title;
+            this.Description = description;
+            this.Year = year;
+            this.Deleted = false;
+        }
+
+        public override string ToString()
+        {
+            var result = "";
+            result += "Gênero: " + this.Genre + Environment.NewLine;
+            result += "Título " + this.Title + Environment.NewLine;
+            result += "Descrição " + this.Description + Environment.NewLine;
+            result += "Ano de Lançamento " + this.Year + Environment.NewLine;
+            return result;
+        }
+
+        public void Delete()
+        {
+            this.Deleted = false;
+        }
+    }
+}
