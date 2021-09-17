df16902fcb3fb55653744381d8fff024591eddbd
Guilherme Melo <gacm@cin.ufpe.br>
2021-09-17 16:54:20 -0300

    Implementing basic IRepository interface

---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Series.Interfaces
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+        
+        T GetById(int id);
+
+        void Insert(T entity);
+
+        void Remove(int id);
+
+        void Update(int id, T entity);
+
+        int NextId();
+    }
+}
