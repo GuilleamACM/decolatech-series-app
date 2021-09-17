5a95274cd730669f3a575806854c56fd874a969c
Guilherme Melo <gacm@cin.ufpe.br>
2021-09-17 17:04:00 -0300

    Implementing SeriesRepositories methods

---
+++ Classes/SeriesRepository.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Series.Interfaces;
+
+namespace Series.Classes
+{
+    public class SeriesRepository : IRepository<Series>
+    {
+        private List<Series> m_seriesList = new List<Series>();
+            
+        public List<Series> List()
+        {
+            return m_seriesList;
+        }
+
+        public Series GetById(int id)
+        {
+            return m_seriesList[id];
+        }
+
+        public void Insert(Series entity)
+        {
+            m_seriesList.Add(entity);
+        }
+
+        public void Remove(int id)
+        {
+            m_seriesList[id].Delete();
+        }
+
+        public void Update(int id, Series entity)
+        {
+            m_seriesList[id] = entity;
+        }
+
+        public int NextId()
+        {
+            return m_seriesList.Count;
+        }
+    }
+}
